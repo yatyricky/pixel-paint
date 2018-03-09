4fb9457b13c7908c89904c041bbe955f18ccfb1d
Rick Sun <yatyricky@gmail.com>
2018-03-09 17:53:12 +0800

    first commit

---
+++ Assets/Scripts/PixelReader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelReader : MonoBehaviour
+{
+    public Texture2D sourceTex;
+    public GameObject PixelsContainerObj;
+    public GameObject TilePrefab;
+
+    void Start()
+    {
+        Color[] pix = sourceTex.GetPixels(0, 0, sourceTex.width, sourceTex.height);
+        //GetComponent<SpriteRenderer>().sprite = Sprite.Create
+
+        for (int i = 0; i < sourceTex.width; i ++)
+        {
+            for (int j = 0; j < sourceTex.height; j ++)
+            {
+                GameObject tile = Instantiate(TilePrefab);
+                SpriteRenderer sr = tile.GetComponent<SpriteRenderer>();
+                Color color = pix[i * sourceTex.width + j];
+                Texture2D destTex = new Texture2D(1, 1);
+                destTex.SetPixels(new Color[] { color });
+                destTex.Apply();
+                sr.sprite = Sprite.Create(destTex, new Rect(0.0f, 0.0f, 1.0f, 1.0f), new Vector2(0.5f, 0.5f), 1);
+                tile.transform.position = new Vector3(j, i, 0f);
+                tile.transform.parent = PixelsContainerObj.transform;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Camera CameraObj;
+    public float perspectiveZoomSpeed = 0.5f;        // The rate of change of the field of view in perspective mode.
+    public float orthoZoomSpeed = 0.5f;        // The rate of change of the orthographic size in orthographic mode.
+
+
+    void Update()
+    {
+        // If there are two touches on the device...
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            Zoom(deltaMagnitudeDiff);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Key pressed");
+            Zoom(10);
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Zoom(-10);
+        }
+    }
+
+    private void Zoom(float delta)
+    {
+        // If the camera is orthographic...
+        if (CameraObj.orthographic)
+        {
+            // ... change the orthographic size based on the change in distance between the touches.
+            CameraObj.orthographicSize += delta * orthoZoomSpeed;
+
+            // Make sure the orthographic size never drops below zero.
+            CameraObj.orthographicSize = Mathf.Max(CameraObj.orthographicSize, 0.1f);
+        }
+        else
+        {
+            // Otherwise change the field of view based on the change in distance between the touches.
+            CameraObj.fieldOfView += delta * perspectiveZoomSpeed;
+
+            // Clamp the field of view to make sure it's between 0 and 180.
+            CameraObj.fieldOfView = Mathf.Clamp(CameraObj.fieldOfView, 0.1f, 179.9f);
+        }
+    }
+}
