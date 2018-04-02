ed8ef6e2e703eace789d6acb9c5971ecd0ecfea1
Rick Sun <yatyricky@gmail.com>
2018-04-02 18:24:45 +0800

    designed hall scene

---
+++ Assets/Scripts/BetterToggleGroup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BetterToggleGroup : MonoBehaviour
+{
+
+    internal void Activate(BetterToggle betterToggle)
+    {
+        foreach (Transform child in transform)
+        {
+            BetterToggle toggle = child.gameObject.GetComponent<BetterToggle>();
+            if (toggle == betterToggle)
+            {
+                toggle.SetEnable();
+            }
+            else
+            {
+                toggle.SetDisable();
+            }
+        }
+    }
+}
